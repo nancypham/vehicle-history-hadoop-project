d4d415c4aaf21dfc859507d2f4e071e3cca3c575
Nancy Pham <16923131+nancypham@users.noreply.github.com>
2021-10-12 17:46:28 -0400

    add execution log, clean up comments

--- src/autoinc_mapper1.py
+++ src/autoinc_mapper1.py
@@ -1,4 +1,5 @@
-#!/user/bin/env python
+#!/Users/nancypham/opt/anaconda3/bin/ python3
+
 import sys
 
 def mapper():
@@ -8,16 +9,31 @@ def mapper():
     Output key: vin_number
     Output value: incident_type, make, year
     '''
-    # Input comes from STDIN (standard input)
     for line in sys.stdin:
-        # if 'q' == line.strip():
-        #     break
         record = line.strip().split(',')
         key = record[2].strip()
         value = record[1].strip(), record[3].strip(), record[5].strip()
         print(f'{key}\t{value}')
 
-    #print('exit')
-
 if __name__ == '__main__':
      mapper()
+
+'''
+Execution log:
+VXIO456XLBB630221	('I', 'Nissan', '2003')
+INU45KIOOPA343980	('I', 'Mercedes', '2015')
+VXIO456XLBB630221	('A', '', '')
+VXIO456XLBB630221	('R', '', '')
+VOME254OOXW344325	('I', 'Mercedes', '2015')
+VOME254OOXW344325	('R', '', '')
+VXIO456XLBB630221	('R', '', '')
+EXOA00341AB123456	('I', 'Mercedes', '2016')
+VOME254OOXW344325	('A', '', '')
+VOME254OOXW344325	('R', '', '')
+EXOA00341AB123456	('R', '', '')
+EXOA00341AB123456	('A', '', '')
+VOME254OOXW344325	('R', '', '')
+UXIA769ABCC447906	('I', 'Toyota', '2017')
+UXIA769ABCC447906	('R', '', '')
+INU45KIOOPA343980	('A', '', '')
+'''
